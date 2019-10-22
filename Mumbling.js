30923f2df4b64a30f18771a88cd08df52c69f45d
Kayla Martinez <kaylamartinez@MacBook-Pro.local>
2019-10-21 20:52:28 -0400

    First commit

---
+++ Mumbling.js
@@ -0,0 +1,10 @@
+function accum(s) {
+	let s.toLowerCase().split('')
+  console.log(letters)
+  for(let i = 0; i < letters.length; i++) {
+    console.log(letters[i])
+    letters[i] = letters[i].toUpperCase() + letters[i].repeat(i)
+    console.log(letters[i])
+  }
+  return letters.join('-')
+}
